5ef75af06f424a0c62f22b6855959f753c8c7651
Mateusz Łuczkowski <mateuszluczkowski@gmail.com>
2021-09-14 11:30:22 +0200

    first init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "styled-components";
+
+const TestComponent = styled.div`
+   width: 100px;
+   height: 100px;
+   font-family: ${({ theme }) => theme.font.family};
+`;
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   return <TestComponent>Hello!</TestComponent>;
 }
 
 export default App;
---
+++ src/utils/theme.js
@@ -0,0 +1,39 @@
+const theme = {
+   font: {
+      size: "15px",
+      family: "'Manrope', sans-serif",
+   },
+   layout: {
+      mobile: "375px",
+      desktop: "1440px",
+   },
+   colors: {
+      primary: {
+         //(Full Slider Bar)
+         softCyan: "hsl(174, 77%, 80%)",
+         //(Slider Backround)
+         strongCyan: "hsl(174, 86%, 45%)",
+         //(Discount Background)
+         lightGrayishRed: "hsl(14, 92%, 95%)",
+         //  (Discount Text)
+         lightRed: "hsl(15, 100%, 70%)",
+         //  (CTA Text)
+         paleBlue: "hsl(226, 100%, 87%)",
+      },
+      neutral: {
+         // (Pricing Component Background)
+         white: "hsl(174, 77%, 80%)",
+         //(Main Background)
+         veryPaleBlue: "hsl(230, 100%, 99%)",
+         //  (Empty Slider Bar)
+         lightGrayishBlue: "hsl(224, 65%, 95%)",
+         // (Toggle Background)
+         lightGrayishBlueSecond: "hsl(223, 50%, 87%)",
+         //  (Text)
+         grayishBlue: "hsl(225, 20%, 60%)",
+         //(Text & CTA Background)
+         darkDesaturatedBlue: "hsl(227, 35%, 25%)",
+      },
+   },
+};
+export default theme;
