71b22f3c28701e800d4f859d1aa0a32e7a55d905
Mateusz Łuczkowski <mateuszluczkowski@gmail.com>
2021-09-15 22:40:17 +0200

    added green background for active toggler

--- src/components/PricingWindow/components/TogglerRow/TogglerRow.js
+++ src/components/PricingWindow/components/TogglerRow/TogglerRow.js
@@ -15,7 +15,7 @@ const TogglerRow = ({ handleToggler, toggler }) => {
          </Toggler>
          <Option>Yearly Billing</Option>
          <DiscountText>{`${
-            windowWidth < 768 ? "25%" : "25% discount"
+            windowWidth < 768 ? "-25%" : "25% discount"
          }`}</DiscountText>
       </WrapperToggler>
    );
